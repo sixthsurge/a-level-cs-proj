3c4fc7a90cf915f1a1cd596b45ff907e0558a3b6
SixthSurge <surgecrafter@gmail.com>
2022-08-27 20:05:57 +0100

    add sfml

--- build.bat
+++ build.bat
@@ -1,9 +1,2 @@
-g++ ^
--O3 ^
--std=c++17 ^
--Iinclude ^
--Ivendor/fmt/include ^
--Ivendor/glm ^
--Ivendor/stb ^
--Ivendor/tinyobjloader ^
-src/main.cpp
+C:/"Program Files"/Git/mingw32/bin/g++ -O3 -std=c++17 -g -DSFML_STATIC -Iinclude -Ivendor/sfml/include -Ivendor/fmt/include -Ivendor/glm -Ivendor/stb -Ivendor/tinyobjloader -c src/main.cpp -o main.o
+C:/"Program Files"/Git/mingw32/bin/g++ -Lvendor/sfml/lib main.o -o run.exe -static-libgcc -static-libstdc++ -lsfml-main -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lopengl32 -lgdi32 -lfreetype -lwinmm
